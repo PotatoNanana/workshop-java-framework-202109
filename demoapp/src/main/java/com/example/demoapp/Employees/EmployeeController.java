4467e897b7180ea04308f9270d6fdfe50cce9b57
K0966131 <napat.j@kbtg.tech>
2021-09-06 15:25:16 +0700

    Working with spring boot test

--- demoapp/src/main/java/com/example/demoapp/Employees/EmployeeController.java
+++ demoapp/src/main/java/com/example/demoapp/Employees/EmployeeController.java
@@ -7,6 +7,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class EmployeeController {
     @GetMapping("/employees")
     public EmployeeResponse[] listEmployee(){
-        return new EmployeeResponse[]{new EmployeeResponse()};
+        EmployeeResponse employee01 = new EmployeeResponse();
+        employee01.setId(1);
+        employee01.setName("Napat01");
+        EmployeeResponse employee02 = new EmployeeResponse();
+        employee02.setId(2);
+        employee02.setName("Napat02");
+        return new EmployeeResponse[]{employee01,employee02};
     }
 }
---
+++ demoapp/src/test/java/com/example/demoapp/Employees/EmployeeControllerTest.java
@@ -0,0 +1,24 @@
+package com.example.demoapp.Employees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class EmployeeControllerTest {
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void listEmployee() {
+        EmployeeResponse[] result = restTemplate.getForObject("/employees",EmployeeResponse[].class);
+
+        // Assert
+        assertEquals(2,result.length);
+        assertEquals(1,result[0].getId());
+        assertEquals("Napat01",result[0].getName());
+    }
+}
