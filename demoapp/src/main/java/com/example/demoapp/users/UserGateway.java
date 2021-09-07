74921eb15a3fdca3e478587c4d962a397cc0842a
K0966131 <napat.j@kbtg.tech>
2021-09-07 16:47:53 +0700

    Create user gateway and generate response class

---
+++ demoapp/src/main/java/com/example/demoapp/users/UserGateway.java
@@ -0,0 +1,12 @@
+package com.example.demoapp.users;
+
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.Target;
+
+@Component
+public class UserGateway {
+    public UserResponse getUserById(int id){
+        return null;
+    }
+}
