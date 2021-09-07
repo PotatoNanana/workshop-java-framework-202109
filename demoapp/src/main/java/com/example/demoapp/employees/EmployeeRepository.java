6ca713bd5369382d6c9b1a99744d11c9611804ba
K0966131 <napat.j@kbtg.tech>
2021-09-07 10:05:13 +0700

    working data with JPA

--- demoapp/src/main/java/com/example/demoapp/employees/EmployeeRepository.java
+++ demoapp/src/main/java/com/example/demoapp/employees/EmployeeRepository.java
@@ -1,6 +1,12 @@
 package com.example.demoapp.employees;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
+    // @Query("select * from employee")
+    List<Employee> findAllByAndName(String name);
+    // select * from employee where id=? and name=?
 }
